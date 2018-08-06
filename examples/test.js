8d1f1339450d3bf2a7ae2f0773b8d8fefa230fb7
Paul Birtle <paul@pbertie.co.uk>
2018-08-06 23:07:44 +0100

    Added update profiles and new colour
    - Expose LUT2 as Light Red
    - Create update profiles for v2
    - Improved the speed of the default update
    - Added command names
    - Put screen into deep sleep after update

--- examples/test.js
+++ examples/test.js
@@ -4,27 +4,38 @@ const inkyphat = require('../lib/inkyphat').getInstance();
   "use strict";
 
   inkyphat.init().then(function() {
-    inkyphat.setBorder(inkyphat.WHITE);
+    inkyphat.setBorder(inkyphat.BLACK);
 
-    let color = inkyphat.BLACK;
-    for (let x = 0; x < (inkyphat.getWidth() / 4) - 0; x++) {
-      for (let y = 1; y < (inkyphat.getHeight() / 4) - 1; y++) {
-        inkyphat.drawRect(x * 4, y * 4, (x * 4) + 4, (y * 4) + 4, color);
-        if (color === inkyphat.BLACK) {
-          color = inkyphat.RED;
-        } else if (color === inkyphat.RED) {
-          color = inkyphat.WHITE;
-        } else {
-          color = inkyphat.BLACK;
-        }
-      }
-    }
+    // Default profile is quick (profiles only work with v2 screen)...
+    // inkyphat.setProfile(inkyphat.profiles.quick);
+    // inkyphat.setProfile(inkyphat.profiles.pimoroni);
+    // inkyphat.setProfile(inkyphat.profiles.noFlash);
+    // inkyphat.setProfile(inkyphat.profiles.clear);
 
-    inkyphat.drawRect(20, 20, 60, 60, inkyphat.BLACK);
+    // let color = inkyphat.BLACK;
+    // for (let x = 0; x < (inkyphat.getWidth() / 4) - 0; x++) {
+    //   for (let y = 1; y < (inkyphat.getHeight() / 4) - 1; y++) {
+    //     inkyphat.drawRect(x * 4, y * 4, (x * 4) + 4, (y * 4) + 4, color);
+    //     if (color === inkyphat.BLACK) {
+    //       color = inkyphat.RED;
+    //     } else if (color === inkyphat.RED) {
+    //       color = inkyphat.WHITE;
+    //     } else {
+    //       color = inkyphat.BLACK;
+    //     }
+    //   }
+    // }
 
-    inkyphat.drawRect(30, 30, 90, 70, inkyphat.RED);
+    // inkyphat.drawRect(150, 20, 210, 60, inkyphat.BLACK);
+    // inkyphat.drawRect(30, 30, 90, 70, inkyphat.RED);
+    // inkyphat.drawRect(90, 100, 160, 70, inkyphat.LIGHT_RED);
+    // inkyphat.drawRect(100, 90, 105, 85, inkyphat.BLACK);
+    // inkyphat.drawRect(60, 5, 80, 50, inkyphat.RED);
 
-    inkyphat.drawRect(21, 40, 80, 50, inkyphat.WHITE);
+    inkyphat.drawRect(5, 25, 210, 40, inkyphat.RED);
+    inkyphat.drawRect(5, 45, 210, 60, inkyphat.LIGHT_RED);
+    inkyphat.drawRect(5, 65, 210, 80, inkyphat.BLACK);
+    inkyphat.drawRect(5, 85, 210, 100, inkyphat.WHITE);
 
     return inkyphat.redraw();
   }).then(function() {
